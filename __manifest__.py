88ace260d1e33d71d595c996f07d68f96955d6c5
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2018-11-06 11:23:23 -0600

    Module creation

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'openacademy',
+    'author': 'Humanytek',
+    'website': 'http://www.humanytek.com',
+    'category': 'Uncategorized',
+    'version': '1.0.0',
+    'depends': [
+    ],
+    'data': [
+    ],
+}
