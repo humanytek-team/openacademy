7ce086f7fca601f2d296e59b9a2cfa552c9a386c
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2018-11-08 15:26:29 -0600

    Exercise 6 - Add a duplicate option

--- models/course.py
+++ models/course.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class Course(models.Model):
@@ -23,6 +23,20 @@ class Course(models.Model):
         inverse_name='course_id',
         string='Sessions',
     )
+
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
 
     _sql_constraints = [
         ('name_description_check',
