8395e8f0c68e8932cf92df9e699a9369f9bc6611
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2018-11-08 15:13:46 -0600

    Add SQL constraints

--- models/course.py
+++ models/course.py
@@ -23,3 +23,13 @@ class Course(models.Model):
         inverse_name='course_id',
         string='Sessions',
     )
+
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         "The title of the course should not be the description"),
+
+        ('name_unique',
+         'UNIQUE(name)',
+         "The course title must be unique"),
+    ]
